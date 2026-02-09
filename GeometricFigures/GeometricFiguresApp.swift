695819c932b71afafcc0dfb9e3407ae9bc27e1f3
Yolanda Piao <ypiao@lcs.on.ca>
2026-02-09 14:25:48 -0500

    Initial Commit

---
+++ GeometricFigures/GeometricFiguresApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometricFiguresApp.swift
+//  GeometricFigures
+//
+//  Created by Yolanda Piao on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct GeometricFiguresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
