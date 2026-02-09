5e6868a1982a470219a780e9ed3b5fe442ee4249
Yolanda Piao <ypiao@lcs.on.ca>
2026-02-09 14:47:53 -0500

    Added output labels for computed properties

--- GeometricFigures/Views/CircleView.swift
+++ GeometricFigures/Views/CircleView.swift
@@ -25,6 +25,10 @@ struct CircleView: View {
             )
             // Label showing the current slider value
             Text("Radius is \(currentCircle.radius.formatted())")
+            
+            Text("Diameter is \(currentCircle.diameter.formatted())")
+            Text("Area is \(currentCircle.area.formatted())")
+            Text("Circumference is \(currentCircle.circumference.formatted())")
         }
     }
 }
